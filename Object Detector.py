31ef78eb046acabd3a0f767bf06e5ba24890ba21
ACuteWoof <63534975+SutharshanVithushan@users.noreply.github.com>
2021-01-01 18:50:31 +0530

    Update Object Detector.py

--- Object Detector.py
+++ Object Detector.py
@@ -1,3 +1,4 @@
+
 # For running inference on the TF-Hub module.
 import tensorflow as tf
 import tensorflow_hub as hub
@@ -122,8 +123,8 @@ def draw_boxes(image, boxes, class_names, scores, max_boxes=10, min_score=0.1):
       np.copyto(image, np.array(image_pil))
   return image
 
-  #Apply Module
-  # By Heiko Gorski, Source: https://commons.wikimedia.org/wiki/File:Naxos_Taverna.jpg
+#Apply Module
+# By Heiko Gorski, Source: https://commons.wikimedia.org/wiki/File:Naxos_Taverna.jpg
 image_url = "https://upload.wikimedia.org/wikipedia/commons/6/60/Naxos_Taverna.jpg" 
 downloaded_image_path = download_and_resize_image(image_url, 1280, 856, True)
 
@@ -158,14 +159,6 @@ def run_detector(detector, path):
 
 run_detector(detector, downloaded_image_path)
 
-image_urls = [
-  # Source: https://commons.wikimedia.org/wiki/File:The_Coleoptera_of_the_British_islands_(Plate_125)_(8592917784).jpg
-  "https://upload.wikimedia.org/wikipedia/commons/1/1b/The_Coleoptera_of_the_British_islands_%28Plate_125%29_%288592917784%29.jpg",
-  # By Américo Toledano, Source: https://commons.wikimedia.org/wiki/File:Biblioteca_Maim%C3%B3nides,_Campus_Universitario_de_Rabanales_007.jpg
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Biblioteca_Maim%C3%B3nides%2C_Campus_Universitario_de_Rabanales_007.jpg/1024px-Biblioteca_Maim%C3%B3nides%2C_Campus_Universitario_de_Rabanales_007.jpg",
-  # Source: https://commons.wikimedia.org/wiki/File:The_smaller_British_birds_(8053836633).jpg
-  "https://upload.wikimedia.org/wikipedia/commons/0/09/The_smaller_British_birds_%288053836633%29.jpg",
-  ]
 
 def detect_img(image_url):
   start_time = time.time()
@@ -174,9 +167,6 @@ def detect_img(image_url):
   end_time = time.time()
   print("Inference time:",end_time-start_time)
 
-detect_img(image_urls[0])
-detect_img(image_urls[1])
-detect_img(image_urls[2])
 
 inp = input("Image Downlaod URL:")
 detect_img(inp)
